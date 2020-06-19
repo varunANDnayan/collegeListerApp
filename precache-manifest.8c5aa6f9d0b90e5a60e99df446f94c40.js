65683eb5e57e1eae065453ea39c414bbf54ffef6
Varun Mukherjee <jacksamuel16@gmail.com>
2020-06-20 00:20:38 +0530

    Updates

---
+++ precache-manifest.8c5aa6f9d0b90e5a60e99df446f94c40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdea3ee2b04291ad4868b7dcfd405397",
+    "url": "/collegeListerApp/index.html"
+  },
+  {
+    "revision": "7cb81a928f669dad3799",
+    "url": "/collegeListerApp/static/css/2.afd40c00.chunk.css"
+  },
+  {
+    "revision": "901ef657bf83d8bd88bc",
+    "url": "/collegeListerApp/static/css/main.48daf24b.chunk.css"
+  },
+  {
+    "revision": "7cb81a928f669dad3799",
+    "url": "/collegeListerApp/static/js/2.69853759.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/collegeListerApp/static/js/2.69853759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "901ef657bf83d8bd88bc",
+    "url": "/collegeListerApp/static/js/main.1009faf6.chunk.js"
+  },
+  {
+    "revision": "f11be45bdb0fc3039ceb",
+    "url": "/collegeListerApp/static/js/runtime-main.073e428f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/deploy-react/precache-manifest.b08de189c9398484393248ce505352ef.js"
+  "/collegeListerApp/precache-manifest.8c5aa6f9d0b90e5a60e99df446f94c40.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deploy-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/collegeListerApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
